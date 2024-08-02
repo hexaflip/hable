2125b9786e8524d80943c233d2b75b84052a2620
Thomas <106038353+hexaflip@users.noreply.github.com>
2024-08-02 13:32:05 -0700

    Error Handling for Habit Creation
    Does not allow the user to create a habit with the same name, and does not allow the user to create a habit with a blank name.

--- ContentView.swift
+++ ContentView.swift
@@ -2,25 +2,29 @@ import SwiftUI
 
 
 struct ContentView: View {
+    //I don't know why this is private or why I use @State lol
     @State private var habits = [String: Int]()
     var body: some View {
         NavigationView {
             VStack {
+                //check if the dictionary is empty
                 if habits.isEmpty {
                     Text("No habits here...").font(.system(size: 25))
                 } else {
+                    // Display Every Habit in their dictionary
                     List {
                         ForEach(Array(habits.keys), id: \.self) { habit in
                             Text(habit)
                         }
                     }
                 }
+                //Redirect the user to the habit creation view
                 NavigationLink(destination: NewHabitView(habits: $habits)){
                     Text("Create a Habit")
                 }
                 
             }
-            .navigationTitle("Habits")
+            .navigationTitle("Hable")
         }
     }
 }
@@ -28,6 +32,7 @@ struct ContentView: View {
 struct NewHabitView: View {
     @Binding var habits: [String: Int]
     @State private var habitName: String = ""
+    @State private var errorMessage: String = ""
     
     var body: some View {
         VStack {
@@ -39,13 +44,23 @@ struct NewHabitView: View {
             .padding()
 
             Button(action: {
-                if !habitName.isEmpty {
+                //make sure the habit name isn't empty lol
+                if habits.keys.contains(habitName)
+                {
+                    errorMessage = "Habit Already Exists"
+                } else if habitName.isEmpty {
+                    errorMessage = "Habit Name cannot be left Blank"
+                } else{
                     habits[habitName] = 0
                 }
             }) {
                 Text("Begin Streak")
             }
-            .padding()
+            if !errorMessage.isEmpty {
+                
+                Text(errorMessage).foregroundStyle(.red)
+                    .padding()
+            }
         }
         .navigationTitle("Create Habit")
     }
